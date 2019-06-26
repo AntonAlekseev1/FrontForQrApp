38b17d18fd2eec66cf9f69ce7559808bd852f06a
Anton <anton_aliakseyeu@senla.eu>
2019-06-26 12:47:44 +0300

    correction 1.6.2

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path =require('path');
 
-app.use(express.static('./dist/app'));
+app.use(express.static('./dist/App'));
 
 app.get('/*', (req, res)=> {
-  res.sendFile(path.join(__dirname,'/dist/app/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/App/index.html'));
 });
 app.listen(process.env.PORT || 8080, () => {
   console.log('Console listening');
