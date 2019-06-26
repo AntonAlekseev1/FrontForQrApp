807306b1c1ab3cb266da3ad4830608dd65e73e22
Anton <anton_aliakseyeu@senla.eu>
2019-06-26 15:08:07 +0300

    added files preview

--- src/app/components/upload-file/upload-file.component.ts
+++ src/app/components/upload-file/upload-file.component.ts
@@ -13,13 +13,31 @@ export class UploadFileComponent implements OnInit {
   selectedFile: File = null;
   responseMessage:string;
   error: Exception;
+  message: string;
+  imgURL: any;
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
   }
 
   onFileSelected(event) {
+    if (event === null || event.length === 0) {
+      return;
+    }
+    var mimeType = event.target.files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+    var reader = new FileReader();
+
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = () => {
+      this.imgURL = reader.result;
+    };
     this.selectedFile = <File>event.target.files[0];
+    this.responseMessage = null;
+    this.message = null;
   }
 
   onUpload() {
